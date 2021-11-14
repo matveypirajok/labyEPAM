d59b85c288ebab65ae4402ef3a2e11cf9500a626
matveypirajok <70639608+matveypirajok@users.noreply.github.com>
2021-11-14 21:16:56 +0300

    Add files via upload

---
+++ 7lab/Aircompany/Aircompany/Planes/MilitaryPlane.cs
@@ -0,0 +1,36 @@
+﻿using Aircompany.Models;
+
+namespace Aircompany.Planes
+{
+    public class MilitaryPlane : Plane
+    {
+        public MilitaryType PlaneType { get; private set; }
+
+        public MilitaryPlane(string model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, MilitaryType PlaneType)
+            : base(model, maxSpeed, maxFlightDistance, maxLoadCapacity)
+        {
+            this.PlaneType = PlaneType;
+        }
+        public override string ToString()
+        {
+            return base.ToString().Replace( ")" , $", PlaneType = {PlaneType} )");
+        }
+        public override bool Equals(object obj)
+        {
+            MilitaryPlane plane = obj as MilitaryPlane;
+            return plane != null &&
+                   base.Equals(obj) &&
+                   PlaneType == plane.PlaneType;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 1701194404;
+            hashCode = hashCode * -1521134295 + base.GetHashCode();
+            hashCode = hashCode * -1521134295 + PlaneType.GetHashCode();
+            return hashCode;
+        }
+
+          
+    }
+}
